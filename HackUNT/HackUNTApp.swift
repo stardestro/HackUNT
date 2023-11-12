98bc3abc49171f4d0ca7e06a41f90db2cc611b1a
stardestro <25290886+stardestro@users.noreply.github.com>
2023-11-11 22:22:53 -0600

    Initial Commit

---
+++ HackUNT/HackUNTApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackUNTApp.swift
+//  HackUNT
+//
+//  Created by Hussain Alkatheri on 11/11/23.
+//
+
+import SwiftUI
+
+@main
+struct HackUNTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
